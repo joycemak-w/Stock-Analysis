81110235e3d26273eafab878c67ce6dcb578db95
joycemak-w <joycemakwaihang128@gmail.com>
2024-09-13 16:25:42 +0800

    update rsi_macd plot

--- rsi_macd.py
+++ rsi_macd.py
@@ -1,4 +1,4 @@
-from stock import rsi_macd_chart
+from stock import rsi_macd_chart, rsi_macd_combine_chart
 
 while True:
     symbols = ["2888.HK", "0005.HK", "2388.HK"]
@@ -8,8 +8,15 @@ while True:
     selected_option = input('Select Option: ')
     # selected_option = int(selected_option)
     if selected_option == '1' or selected_option == '2' or selected_option == '3':
+        print('1. Show combined chart \n2. Show both rsi and macd chart')
+        show_all = input('Select Option: ')
         selected_option = int(selected_option)
-        rsi_macd_chart(symbols[selected_option-1])
+        if show_all == '1':
+            rsi_macd_combine_chart(symbols[selected_option-1])
+        elif show_all == '2':
+            rsi_macd_chart(symbols[selected_option-1])
+        else:
+            print('Wrong Option')
     elif selected_option == '4':
         break
     else:
--- stock.py
+++ stock.py
@@ -189,7 +189,7 @@ def rsi_macd_chart(selected_symbol):
     
     # First chart:
     # Plot the closing price on the first chart
-    ax1.set_title('RSI and MACD')
+    ax1.set_title(f'RSI and MACD of {symbols[selected_symbol]}')
     # df['label'] = np.where((rsi<70)&(rsi>30), 1, -1)
     df['label'] = np.where((rsi<70), 1, -1)
     def plot_func(group):
@@ -205,7 +205,7 @@ def rsi_macd_chart(selected_symbol):
 
     # Second chart
     # Plot the RSI
-    ax2.set_title('Relative Strength Index')
+    ax2.set_title(f'Relative Strength Index of {symbols[selected_symbol]}')
     ax2.plot(rsi, color='orange', linewidth=1)
     # Add two horizontal lines, signalling the buy and sell ranges.
     # Oversold
@@ -215,7 +215,7 @@ def rsi_macd_chart(selected_symbol):
 
     # Third chart
     # Plot the MACD
-    ax3.set_title('Moving Average Convergence/Divergence')
+    ax3.set_title(f'Moving Average Convergence/Divergence of {symbols[selected_symbol]}')
     # fast line: 12days EMA - 26days EMA
     # EMA: higher weighting on recent price SMA: same weighting
     ax3.plot(df['MACD'], label='MACD Line', color='blue', alpha=0.5, linewidth=1)
@@ -224,8 +224,8 @@ def rsi_macd_chart(selected_symbol):
     ax3.bar(df.index, df['MACD_Diff'], label='Histogram', color='grey', alpha=0.5)
 
     # Markers for bullish and bearish crossover
-    ax3.scatter(df.index[df['Golden_Cross']], df['MACD'][df['Golden_Cross']], marker='^', color='g', label='Bullish Crossover')
-    ax3.scatter(df.index[df['Death_Cross']], df['MACD'][df['Death_Cross']], marker='v', color='r', label='Bearish Crossover')
+    ax3.scatter(df.index[df['Golden_Cross']], df['MACD'][df['Golden_Cross']], marker='^', color='g', label='Golden Cross')
+    ax3.scatter(df.index[df['Death_Cross']], df['MACD'][df['Death_Cross']], marker='v', color='r', label='Death Cross')
     ax3.legend()
     # Display the charts
     plt.show()
@@ -235,3 +235,77 @@ def rsi_macd_chart(selected_symbol):
 # rsi_macd_chart('2888.HK')
 
 
+def rsi_macd_combine_chart(selected_symbol):
+    query = "SELECT [date],[close] FROM Stocks WHERE symbol = '%s'" % selected_symbol 
+    df = pd.read_sql(query, conn)
+    # df.to_csv('./rsi_macd.csv', index=0)
+    # df = pd.read_csv("./rsi_macd.csv") 
+    df['date']=pd.to_datetime(df['date'])
+    df = df.set_index('date')
+    #RSI
+    change = df["close"].diff()
+    change.dropna(inplace=True)
+
+    # Create two copies of the Closing price Series
+    change_up = change.copy()
+    change_down = change.copy()
+
+    change_up[change_up<0] = 0
+    change_down[change_down>0] = 0
+
+    # Verify that we did not make any mistakes
+    change.equals(change_up+change_down)
+
+    # Calculate the rolling average of average up and average down
+    avg_up = change_up.rolling(14).mean()
+    avg_down = change_down.rolling(14).mean().abs()
+
+    rsi = 100 * avg_up / (avg_up + avg_down)
+    rsi[df.index[0]] = 0.0
+    rsi = rsi.sort_index()
+    # new_row = pd.DataFrame([0.0], index=(df.index[0]))
+    # rsi = pd.concat([new_row, rsi]).sort_index()
+    rsi = rsi.fillna(0)
+    # print(rsi)
+
+    #MACD
+    macd_object = ta.trend.MACD(df['close'])
+    df['MACD'] = macd_object.macd()
+    df['MACD_Signal'] = macd_object.macd_signal()
+    df['MACD_Diff'] = macd_object.macd_diff()
+
+    # Identify starting points of bullish and bearish trends
+    df['Bullish_Run_Start'] = (df['MACD'] > df['MACD_Signal']) & (df['MACD'].shift(1) <= df['MACD_Signal'].shift(1))
+    df['Bearish_Run_Start'] = (df['MACD'] < df['MACD_Signal']) & (df['MACD'].shift(1) >= df['MACD_Signal'].shift(1))
+    # print(type(rsi.index[0]))
+
+    # Identify gold and death cross points
+    df['Golden_Cross'] = (df['MACD'] > df['MACD_Signal']) & (df['MACD'].shift(1) <= df['MACD_Signal'].shift(1))
+    df['Death_Cross'] = (df['MACD'] < df['MACD_Signal']) & (df['MACD'].shift(1) >= df['MACD_Signal'].shift(1))
+
+    # Create two charts on the same figure.
+    plt.figure(figsize=(10,8))
+    ax1 = plt.subplot()
+    # fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))
+    
+    # First chart:
+    # Plot the closing price on the first chart
+    ax1.set_title(f'RSI and MACD of {symbols[selected_symbol]}')
+    # df['label'] = np.where((rsi<70)&(rsi>30), 1, -1)
+    df['label'] = np.where((rsi<70), 1, -1)
+    def plot_func(group):
+        color = 'r' if (group['label'] < 0).all() else 'g'
+        lw = 2.0
+        ax1.plot(group.index, group.close, c=color, linewidth=lw)
+
+    df.groupby((df['label'].shift() * df['label'] < 0).cumsum()).apply(plot_func)
+
+    ax1.scatter(df.index[df['Bullish_Run_Start']], df['close'][df['Bullish_Run_Start']], marker='^', color='#FFD700', label='Start Bullish Run' ,zorder=2.5)
+    ax1.scatter(df.index[df['Bearish_Run_Start']], df['close'][df['Bearish_Run_Start']], marker='v', color='#7e1300', label='Start Bearish Run' ,zorder=2.5)
+    ax1.legend()
+
+    # Display the charts
+    plt.show()
+    # return fig
+
+# rsi_macd_combine_chart('2888.HK')
